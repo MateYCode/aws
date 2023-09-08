e4df0f6410ff9401c45a8261b1bd7be86b0a6cdf
ignacio <nacho>
2023-09-08 17:54:45 -0300

    logged sessionToken

--- signature/index.js
+++ signature/index.js
@@ -11,20 +11,21 @@ const client = new STSClient({ region });
 		const command = new AssumeRoleCommand({
 			RoleArn: '',
 			RoleSessionName: 'DelegatedSession',
-			DurationSeconds: 900,
+			DurationSeconds: 1800,
 		});
 		const response = await client.send(command)
 
 		const accessKeyId = response.Credentials.AccessKeyId;
 		const secretAccessKey = response.Credentials.SecretAccessKey;
-		
+		const sessionToken = response.Credentials.SessionToken
+
 		// We update the x-amz-credential in the policy, passing the current access id 
-		const credentialObj = policy.conditions.find(condition => condition["x-amz-credential"]);  
-		credentialObj["x-amz-credential"] = credentialObj["x-amz-credential"].replace(/{accessId}/, accessKeyId); 
-		
+		const credentialObj = policy.conditions.find(condition => condition["x-amz-credential"]);
+		credentialObj["x-amz-credential"] = credentialObj["x-amz-credential"].replace(/{accessId}/, accessKeyId);
+
 		// We create a base64 string from the policy
 		const stringToSign = btoa(JSON.stringify(policy));
-		
+
 		const date = '20230908';
 		const service = 's3';
 		const dateKey = crypto.createHmac('sha256', `AWS4${secretAccessKey}`).update(date).digest();
@@ -32,8 +33,9 @@ const client = new STSClient({ region });
 		const dateRegionServiceKey = crypto.createHmac('sha256', dateRegionKey).update(service).digest();
 		const signingKey = crypto.createHmac('sha256', dateRegionServiceKey).update('aws4_request').digest();
 		const signature = crypto.createHmac('sha256', signingKey).update(stringToSign).digest('hex');
-		
+
 		console.log('This is the accessKeyId: ', accessKeyId, '\n');
+		console.log('This is the sessionToken', sessionToken);
 		console.log('This is the signature', signature);
 
 	} catch (error) {
